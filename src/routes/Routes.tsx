442e542534326818bb9bf83814bc0cce8a4353bd
Rafael Domenegueti <rafael.domenegueti@ecenture.com.br>
2022-07-23 13:52:46 -0300

    Fix: Configuring the project

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -5,14 +5,12 @@ import {
 } from "react-router-dom";
 
 import { Home } from "../views/Home";
-import { Login } from "../views/Login";
 
 export const Routes = () => {
 	return (
 		<BrowserRouter>
 			<Router>
 				<Route path="/" element={<Home />} />
-				<Route path="/login" element={<Login />} />
 			</Router>
 		</BrowserRouter>
 	);
